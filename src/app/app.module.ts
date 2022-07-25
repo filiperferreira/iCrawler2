48feb1ebe60780120028ebaabed4b97af6b1fbeb
Filipe Ferreira <frferreira9@gmail.com>
2022-07-25 14:50:16 -0700

    Removed deprecated pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
-import { StatPipePipe } from './stat-pipe.pipe';
 import { DungeonComponent } from './dungeon/dungeon.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +14,6 @@ import { LogWindowComponent } from './log-window/log-window.component';
   declarations: [
     AppComponent,
     PlayerComponent,
-    StatPipePipe,
     DungeonComponent,
     InventoryComponent,
     CombatComponent,
--- src/app/stat-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StatPipePipe } from './stat-pipe.pipe';
-
-describe('StatPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new StatPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
