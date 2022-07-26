2fd11273ecd3e53c1b37b47851ed2e88554ac063
Filipe Ferreira <frferreira9@gmail.com>
2022-07-26 10:17:10 -0700

    Added save/load feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { DungeonDataService } from './dungeon/dungeon-data/dungeon-data.service';
+import { PlayerDataService } from './player/player-data/player-data.service';
+import { InventoryDataService } from './inventory/inventory-data/inventory-data.service';
 
 @Component({
   selector: 'app-root',
@@ -20,13 +22,51 @@ export class AppComponent {
     Breakpoints.XSmall
   ])
 
-  constructor(private breakpointObserver: BreakpointObserver, private dungeonData: DungeonDataService) { }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private playerData: PlayerDataService,
+    private dungeonData: DungeonDataService,
+    private inventoryData: InventoryDataService
+  ) { }
 
   ngOnInit(): void {
     this.breakpoint$.subscribe(() => this.breakpointChanged());
+    if (localStorage.length != 0) {
+      this.loadGame();
+    }
+    else {
+      console.log("hello");
+    }
   }
 
-  private breakpointChanged() {
+  clearSave(): void {
+    localStorage.clear();
+  }
+
+  loadGame(): void {
+    var loader = localStorage.getItem('player');
+    if (loader) {
+      this.playerData.player = JSON.parse(loader);
+    }
+
+    loader = localStorage.getItem('dungeon');
+    if (loader) {
+      this.dungeonData.dungeon = JSON.parse(loader);
+    }
+
+    loader = localStorage.getItem('inventory');
+    if (loader) {
+      this.inventoryData.inventory = JSON.parse(loader);
+    }
+  }
+
+  saveGame(): void {
+    localStorage.setItem('player', JSON.stringify(this.playerData.player));
+    localStorage.setItem('dungeon', JSON.stringify(this.dungeonData.dungeon));
+    localStorage.setItem('inventory', JSON.stringify(this.inventoryData.inventory));
+  }
+
+  private breakpointChanged(): void {
     if (this.breakpointObserver.isMatched(Breakpoints.XLarge)) {
       this.currentBreakpoint = Breakpoints.XLarge;
     }
